442e18c90843c8d18833eb55ca2fb292847fb6c2
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2025-03-02 00:37:29 +0100

    create auth fork screen and navigation to sub auth screens

---
+++ app/Authentication/signup.tsx
@@ -0,0 +1,10 @@
+
+
+import  {View,Text} from "react-native"
+
+
+export default function Signup(){
+    return <View>
+                <Text>This is the signup page</Text>
+            </View>
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,71 @@
-import { Text, View,StyleSheet } from "react-native";
+import styled from "styled-components/native";
+import {StyleSheet,SafeAreaView,View } from "react-native";
+import {Image} from "expo-image";
+import {Button} from "react-native-paper";
+import { Link } from "expo-router";
 
-import Authentication from "./UI/Authentication";
+const landingImage  = require("../assets/images/hicon.png")
+
+const Container  = styled.View`
+`
+
 
 export default function Index() {
-  return <View style={Style.mainView}>
-            <Authentication/>
-          </View>
+
+    return <>
+              <SafeAreaView/>
+              <Container style={styles.mainContainer}>
+                <Image style={styles.image} source={landingImage} />
+                <View style={styles.buttonsContainer}>
+                    <Link style={styles.buttonLinkStyle} href={"/Authentication/signup"}>
+                      <Button mode="contained" textColor="white" style = {{...styles.buttons,...styles.signupButton}} >
+                        Sign Up
+                      </Button>
+                    </Link>
+                    <Link style={styles.buttonLinkStyle} href={"/Authentication/login"}>
+                      <Button mode="contained"  textColor="white"   style = {{...styles.buttons,...styles.loginButton}}>Login</Button>
+                    </Link>
+                </View>
+              </Container>
+            </>
 }
 
-const Style = StyleSheet.create({
-  mainView:{
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  }
+
+const styles = StyleSheet.create({
+  image:{
+    objectFit:"fill",
+    width:250,
+    height:250,
+    marginTop:150
+  },
+  buttonLinkStyle:{
+    width:"85%"
+  },
+  buttonsContainer:{
+    width:"100%",
+    flex:1,
+    alignItems:"center",
+    gap:20,
+  },
+  buttons:{
+    padding:16,
+    fontSize:24,
+    width:"100%"
+  },
+  signupButton:{
+    backgroundColor:"black"
+  },
+  loginButton:{
+    // borderColor:"black",
+    // borderStyle:"solid",
+    backgroundColor:"#dd571c",
+    // borderWidth:2,
+    // color:"black"
+  },
+  mainContainer:{
+    flex:1,
+    gap:100,
+    alignItems:"center",
+    padding:36,
+    backgroundColor:"white"}
 })
