6fd332c47e114acb86c12bbb87cea58067f27676
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2025-03-08 18:10:38 +0100

    componentize landing page fork

--- app/index.tsx
+++ app/index.tsx
@@ -1,70 +1,8 @@
-import styled from "styled-components/native";
-import {StyleSheet,SafeAreaView,View } from "react-native";
-import {Image} from "expo-image";
-import {Button} from "react-native-paper";
-import { Link } from "expo-router";
-
-const landingImage  = require("../assets/images/hicon.png")
-
-const Container  = styled.View`
-`
+import LandingFork from "./landingFork"
 
 
 export default function Index() {
 
-    return <>
-              <SafeAreaView/>
-              <Container style={styles.mainContainer}>
-                <Image style={styles.image} source={landingImage} />
-                <View style={styles.buttonsContainer}>
-                    <Link style={styles.buttonLinkStyle} href={"/Authentication/signup"}>
-                      <Button  mode="contained" textColor="white" style = {{...styles.buttons,...styles.signupButton}} >
-                        Sign Up
-                      </Button>
-                    </Link>
-                    <Link style={styles.buttonLinkStyle} href={"/Authentication/login"}>
-                      <Button  mode="contained"  textColor="white"   style = {{...styles.buttons,...styles.loginButton}}>Login</Button>
-                    </Link>
-                </View>
-              </Container>
-            </>
+    return <LandingFork/>
 }
 
-
-const styles = StyleSheet.create({
-  image:{
-    objectFit:"fill",
-    width:300,
-    height:300,
-    marginTop:"20%"
-  },
-  buttonLinkStyle:{
-    width:"100%"
-  },
-  buttonsContainer:{
-    width:"100%",
-    flex:1,
-    alignItems:"center",
-    gap:36,
-  },
-  buttons:{
-    padding:8,
-    // fontSize:48,
-    width:"100%",
-    height:74,
-    borderRadius:12,
-    boxSizing:"content-box"
-  },
-  signupButton:{
-    backgroundColor:"black"
-  },
-  loginButton:{
-    backgroundColor:"#ec7d3c",
-  },
-  mainContainer:{
-    flex:1,
-    gap:"10%",
-    alignItems:"center",
-    padding:36,
-    backgroundColor:"white"}
-})
